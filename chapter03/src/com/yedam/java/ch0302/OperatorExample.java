393b14410718eae71314e18dc2d7ab02bde1cb92
admin <tnstlazz123@naver.com>
2022-11-04 11:58:11 +0900

    2022/11/04 second commit

---
+++ chapter03/src/com/yedam/java/ch0302/OperatorExample.java
@@ -0,0 +1,25 @@
+package com.yedam.java.ch0302;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		//부호연산자 
+		int a = -100;
+		int result1 = +a;
+		int result2 = -a;
+		System.out.println("result1:" + result1);
+		System.out.println("result2:" + result2);
+		//  -- >  + / ++ > -
+		
+		byte b = 100;
+		//byte result3 = (byte)-b;
+		int result3 = -b;
+		System.out.println("result3:" + result3);
+		// 변수에 노란줄은 선언은 되었으나, 사용은 안한상태. 
+		
+		
+		
+
+	}
+
+}
